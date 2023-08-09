b56ef1e9aba3194578ae3232bf157634da35e5f7
Darcy Linde <darcy.linde@digitalcrafts.com>
2023-08-09 11:08:36 -0400

    fix example 2

--- example-2.js
+++ example-2.js
@@ -1,7 +1,10 @@
 function sum_char_codes(str) {
   let sum = 0;
   for (let i = 0; i < str.length; ++i) {
-    sum += n.charCodeAt(i);
+    sum += str.charCodeAt(i);
+  }
+  for (let i = 0; i < str.length; ++i) {
+    sum += str.charCodeAt(i);
   }
 
   return sum;
